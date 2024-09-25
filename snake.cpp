1dc1f0deaeac9e0047cac428195d5385f6b32863
Faria-Zaman-Oiky <zamanfaria29@gmail.com>
2024-09-25 22:30:42 +0600

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -79,7 +79,7 @@ public:
 
     bool move(Point food){
 
-        for(int i= length-1;i>0;i--)  // lenght = 4
+        for(int i= length-1;i>0;i--)  
         {
             body[i] = body[i-1];
         }
